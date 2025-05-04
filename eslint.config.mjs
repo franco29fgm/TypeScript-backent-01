af79e433fd916c377ce47eb0c4203c108d7fbe7b
Franco@DENA <franco29fgm@gmail.com>
2025-05-04 11:52:52 -0500

    agregar objetos iniciales

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  {
+    rules:{
+      "no-unused-vars": "warn",
+			"no-console": "error",
+    }
+  }
+]);
