c2b6629812460c867a7f220b20e2625bfccba433
Franco@DENA <franco29fgm@gmail.com>
2025-05-05 23:23:40 -0500

    Configuracion de hussky para un revisión previa a los commit

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hola Franco!!");
+        res.send("Estamos casi listos");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
